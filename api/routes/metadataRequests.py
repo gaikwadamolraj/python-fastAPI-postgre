4e96be8e571bde829c40152faa851d8f8dabfaef
Amol Gaikwad <gaikwadamolraj@gmail.com>
2023-11-02 13:28:35 +0000

    Handle exception for byid call

--- api/routes/metadataRequests.py
+++ api/routes/metadataRequests.py
@@ -10,14 +10,22 @@ from controller.metadataRequestController import create, fetchById, fetchAll
 
 router = APIRouter(tags=["metadatas"])
 
+
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=MetadataResponse)
 async def create_request(metaReq: MetadataRequest, db: Session = Depends(get_db)):
     return create(db=db, metaReq=metaReq)
+
 
 @router.get("/all", status_code=status.HTTP_200_OK, response_model=List[MetadataResponse])
 async def get_all_requests(db: Session = Depends(get_db)):
     return fetchAll(db=db)
 
+
 @router.get("/{id}", status_code=status.HTTP_200_OK, response_model=MetadataResponse)
 async def get_request(id, db: Session = Depends(get_db)):
-    return fetchById(db=db, id=id)
+    try:
+        return fetchById(db=db, id=id)
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Meatadata Request not found"
+        )
